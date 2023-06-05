e6f13dea29a6b54952b36afb1a0aff09634aaec1
WirdRidov <sametkula8@mail.ru>
2023-06-05 09:00:19 +0300

    feat: new codes added two number are adding a sum holder

--- enYuksekSayiyibul.py
+++ enYuksekSayiyibul.py
@@ -1,7 +1,15 @@
 
 def function(x)
     result = 1
-    for i = 1: in x
+    for i = 1 in x
         result *= i
         
     return result
+
+
+num1 = 1.5
+num2 = 2.5
+
+sum = num1 + num2
+
+print('The sum of {0} , {1} these numbers is {2}'.format(num1, num2, sum))
