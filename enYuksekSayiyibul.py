68cbc343d67bf5524d8d99a9f59511a41cf9d4c6
WirdRidov <sametkula8@mail.ru>
2023-06-05 09:22:55 +0300

    feat(code): added new things on code space

--- enYuksekSayiyibul.py
+++ enYuksekSayiyibul.py
@@ -13,3 +13,11 @@ num2 = 2.5
 sum = num1 + num2
 
 print('The sum of {0} , {1} these numbers is {2}'.format(num1, num2, sum))
+
+
+num3 = input('enter a number: ')
+num4 = input('enter a second number: ')
+
+sum = num3 + num4
+
+print('The sum of {0} , {1} these numbers is {2}'.format(num3, num4, sum))
